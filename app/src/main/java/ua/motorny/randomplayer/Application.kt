ca5aa022609ec1610983a855bc4ea95fe1364eb3
motorniy <j.motorniy@gmail.com>
2016-03-15 19:48:35 +0200

    vksdk login work as expected, retrieving music also works fine

--- app/src/main/java/ua/motorny/randomplayer/Application.kt
+++ app/src/main/java/ua/motorny/randomplayer/Application.kt
@@ -1,9 +1,7 @@
 package ua.motorny.randomplayer
 
 import com.vk.sdk.VKSdk
-import org.androidannotations.annotations.EApplication
 
-@EApplication
 open class Application : android.app.Application() {
 
     override fun onCreate() {
